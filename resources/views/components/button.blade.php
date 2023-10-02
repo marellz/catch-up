67d764f1d86eed63635235dbe0209623766f15a4
Dave Njoroge <d.njoroge@thecodingmachine.com>
2023-10-02 14:22:16 +0300

    update button component, apply in the other components

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,9 +1,9 @@
-@props(['type' => ''])
+@props(['type' => '', 'color' => 'bg-dark-blue hover:bg-light-blue hover:text-dark-blue text-white'])
 
 <button
     {{ $attributes->merge([
         'type' => $type,
-        'class' => 'inline-flex justify-center items-center py-2 px-5 bg-dark-blue text-white rounded',
+        'class' => 'inline-flex justify-center items-center py-2 px-5 rounded space-x-2'.' '.$color,
     ]) }}>
 
     {{ $slot }}
--- resources/views/components/upcoming-task.blade.php
+++ resources/views/components/upcoming-task.blade.php
@@ -2,7 +2,7 @@
 
 <div
     class="bg-white shadow p-5 rounded border-l-4 @if ($task->status_id == 1) border-light-blue @elseif($task->status_id == 2) border-blue-alt @endif ">
-    <div class="flex items-start xl:items-center">
+    <div class="flex items-start lg:items-center">
         <div class="flex flex-col xl:flex-row items-start xl:items-center xl:flex-auto">
             <div class="flex space-x-3 items-center">
                 <h1 class="text-xl font-medium">{{ $task->name }}</h1>
@@ -72,12 +72,11 @@
                 </div>
             </div>
         </div>
-        <div class="flex space-x-2 md:py-0 ml-auto transform -translate-y-2">
-
+        <div class="flex space-x-2 md:py-0 ml-auto transform -translate-y-2 lg:-translate-y-0">
             <form action="{{ route('tasks.destroy', $task->id) }}" method="POST">
                 @method('delete')
                 @csrf
-                <button class="p-2 text-red hover:bg-red hover:text-white flex items-center space-x-1 rounded">
+                <x-button color="text-red hover:bg-red hover:text-white">
 
                     {{-- trash --}}
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -86,7 +85,7 @@
                             d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                     </svg>
                     <span class="hidden lg:inline">Delete</span>
-                </button>
+                </x-button>
             </form>
         </div>
 
