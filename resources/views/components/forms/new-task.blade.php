958f991976aae7a025885c4da78e2d269fdec3e2
Dave Njoroge <d.njoroge@thecodingmachine.com>
2023-10-04 01:10:30 +0300

    few ux updates

--- resources/views/components/forms/new-task.blade.php
+++ resources/views/components/forms/new-task.blade.php
@@ -2,10 +2,15 @@
 
 <form action="{{ route('tasks.create') }}" method="POST">
     @csrf
-    <div class="bg-white shadow rounded p-5">
-        <h1 class="text-xl font-semibold mb-6">Add a new task</h1>
 
-        <div class="grid lg:grid-cols-2 gap-4">
+    <div class="bg-white shadow rounded p-5" x-data="{ open: {{ $errors->count() }}, more: false }">
+
+        <button type="button" class="flex items-center w-full text-left" @click="open=!open">
+            <h1 class="flex-auto text-xl font-semibold">Add a new task</h1>
+            <x-icons.plus />
+        </button>
+
+        <div class="grid lg:grid-cols-2 gap-4 mt-6" x-show="open">
 
             <x-form-input label="Task name" name="name" class="lg:col-span-2" />
 
@@ -27,7 +32,16 @@
                 <input type="date" class="px-2 py-1.5 rounded w-full border border-grey" name="due_date"
                     value="{{ now()->addDay() }}" id="task-due" />
             </div>
+        </div>
 
+        <div class="mt-5" x-show="open">
+            <button type="button" class="flex items-center text-left space-x-4 text-dark-blue" @click="more=!more">
+                <h1 class="flex-auto text-lg font-semibold ">More details</h1>
+                <x-icons.chevron-down />
+            </button>
+        </div>
+
+        <div class="grid lg:grid-cols-2 gap-4 mt-6 pb-2" x-show="more">
             <div class="">
                 <x-dropdown label="Categories">
                     @foreach ($categories as $option)
@@ -39,27 +53,29 @@
                         </div>
                     @endforeach
                 </x-dropdown>
-                
+
             </div>
 
             <div class="">
                 <x-dropdown label="Assignees">
                     @foreach ($contacts as $option)
-                       <div class="flex px-2 hover:bg-grey">
-                           <input type="checkbox" name="assignees[]" id="task-assignee-{{ $option->id }}"
-                               value={{ $option->id }} />
-                           <label class="py-2 flex-auto px-2" for="task-assignee-{{ $option->id }}">
-                               {{ $option->name }}</label>
-                       </div>
-                   @endforeach
-               </x-dropdown>
+                        <div class="flex px-2 hover:bg-grey">
+                            <input type="checkbox" name="assignees[]" id="task-assignee-{{ $option->id }}"
+                                value={{ $option->id }} />
+                            <label class="py-2 flex-auto px-2" for="task-assignee-{{ $option->id }}">
+                                {{ $option->name }}</label>
+                        </div>
+                    @endforeach
+                </x-dropdown>
             </div>
 
             <div class="flex flex-col lg:col-span-2">
                 <label for="task-description" class="mb-2">Description</label>
                 <textarea class="p-2 w-full border border-grey rounded" name="description" id="task-description"></textarea>
             </div>
+        </div>
 
+        <div class="p-4" x-show="open">
             <div class="text-end lg:col-span-2">
                 <x-button>
                     <x-icons.plus />
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,14 @@
             <h1 class="text-3xl">
                 Hello {{ $user->name }}.
             </h1>
-            <p class="mt-2 opacity-50">All your tasks.</p>
+            <p class="mt-2 opacity-50" x-data="{ date: new Date() }">Today is 
+                <span class="font-medium" x-text="date.toLocaleDateString('en-UK', {
+                    weekday: 'long',
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric'
+                })"></span>
+            </p>
         </div>
         <div class="grid lg:grid-cols-3 gap-10 mb-10 items-start">
             <div class="lg:col-span-2 px-4">
@@ -32,23 +39,27 @@
                         <x-task.upcoming-task :task="$task" />
                     @empty
                         <div class="py-10 border rounded border-grey">
-                            <h1 class="text-center text-dark-blue">All caught up!</h1>
+                            <h1 class="text-center text-dark-blue">No upcoming tasks.</h1>
                         </div>
                     @endforelse
                 </div>
 
-                <div class="space-y-3 mt-10">
-                    <h1 class="text-xl font-semibold mb-6">Assigned to you</h1>
+                @if($assigned->count())
 
-                    {{-- tasks loop --}}
-                    @forelse($assigned as $task)
-                        <x-task.assigned-task :task="$task" />
-                    @empty
-                        <div class="py-10 border rounded border-grey">
-                            <h1 class="text-center text-dark-blue">All caught up!</h1>
-                        </div>
-                    @endforelse
-                </div>
+                    <div class="space-y-3 mt-10">
+                        <h1 class="text-xl font-semibold mb-6">Assigned to you</h1>
+
+                        {{-- tasks loop --}}
+                        @forelse($assigned as $task)
+                            <x-task.assigned-task :task="$task" />
+                        @empty
+                            <div class="py-10 border rounded border-grey">
+                                <h1 class="text-center text-dark-blue">All caught up!</h1>
+                            </div>
+                        @endforelse
+                    </div>
+
+                @endif
             </div>
             <div class="px-4 lg:px-0">
                 <div class="bg-white shadow rounded p-4">
@@ -58,7 +69,7 @@
                         <x-task.completed-task :task="$task" />
                     @empty
                         <div class="py-10 text-center text-light-blue border rounded">
-                            <p> Empty! </p>
+                            <p> No completed tasks yet. </p>
                         </div>
                     @endforelse
                 </div>
